6d6a7e59ff04356745d2d6980d90a0ba11297c7a
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-07-24 23:40:02 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,30 @@
 
 $(document).ready(function(){
 
-// call functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  }
+});
+
+function frameIt() {
+  $('img').on('load', function() {
+    ($( "img" ).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if(key.which == 'G'){
+        alert('G was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+  });
+}
 
 });
